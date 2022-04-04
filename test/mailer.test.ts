c85b12c53029258d4202fa79fd77b9fe9b1e312e
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-04 13:01:32 +0200

    add: rating feedback route

--- test/mailer.test.ts
+++ test/mailer.test.ts
@@ -1,6 +1,40 @@
-export { };
-describe('Testing nodemialer', () => {
-    it('should test ', () => {
-        expect(true).toBe(true);
-    })
+export {};
+import email from "../pages/api/email";
+
+describe("Testing nodemialer", () => {
+  it("should email validation validate email", () => {
+    const req = {
+      body: JSON.stringify({
+        discount: 0.2,
+        tax: 0.06,
+        items: [
+          {
+            id: 1,
+            price: 19.99,
+            quantity: 2,
+          },
+          {
+            id: 2,
+            price: 43.49,
+            quantity: 1,
+          },
+        ],
+      }),
+    };
+
+    const json = jest.fn();
+
+    const status = jest.fn(() => {
+      return {
+        json,
+      };
+    });
+
+    const res = {
+      status,
+    };
+    // email(req, res);
+    expect(true).toBe(true);
+    expect(false).not.toBe(true);
+  });
 });
