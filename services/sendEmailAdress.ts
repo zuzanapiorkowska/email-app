975922a8a84fba3b43b672eb2f8d9fe9d3bfe31e
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-04 13:24:36 +0200

    clean up

---
+++ services/sendEmailAdress.ts
@@ -0,0 +1,19 @@
+import { EmailAnswer } from "../interfaces/email";
+import axios from "axios";
+
+
+export class sendEmailAdress {
+ async send(
+    emailAdress: string
+  ): Promise<EmailAnswer> {
+    const url = "http://localhost:3000/email/";
+    try {
+      console.log("tried to send");
+      const response = await axios.post(url, { email: emailAdress });
+      const ConfirmationAnswer: EmailAnswer = response.data;
+      return ConfirmationAnswer;
+    } catch (err) {
+      throw new Error(err.message + " Cannot connect to http://localhost:3000/email/");
+    }
+  }
+}
