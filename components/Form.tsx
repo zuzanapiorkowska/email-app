20a7f2829e4be305d4c238b7b9ce388cb9cddb14
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-04 15:26:55 +0200

    remove unUsed things

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,29 +1,25 @@
 import { Button } from "./Button";
 import { Input } from "./Input";
-import { Formik, Field, useFormik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useState } from "react";
-import { EmailAnswer, EmailForm } from "../interfaces/email";
-import { mockSendEmailAdress } from "../test/mockSendEmailAdress";
+import { EmailAnswer } from "../interfaces/email";
 import { sendEmailAdress } from "../services/sendEmailAdress";
 import { Answer } from "./Answer";
 
 export function Form() {
-  const [email, setEmail] = useState<string>("");
   const [response, setResponse] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
   const SignupSchema = Yup.object().shape({
     email: Yup.string()
       .email("Niepoprawny adres e-mail")
       .required("Wpisz swój e-mail!"),
   });
 
-  const { values, handleChange, handleSubmit, handleBlur, errors } = useFormik({
+  const { values, handleChange, handleSubmit, errors } = useFormik({
     initialValues: { email: "" },
     validationSchema: SignupSchema,
     onSubmit: (values: { email: string }): void => {
       console.log("Oto email", values.email);
-      setEmail(values.email);
       new sendEmailAdress()
         .send(values.email)
         .then((res: EmailAnswer) => {
