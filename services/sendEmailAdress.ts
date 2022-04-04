d8dce354b3575cecb8996f6976b95c491faaec56
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-04 13:31:14 +0200

    change endpoint

--- services/sendEmailAdress.ts
+++ services/sendEmailAdress.ts
@@ -6,14 +6,14 @@ export class sendEmailAdress {
  async send(
     emailAdress: string
   ): Promise<EmailAnswer> {
-    const url = "http://localhost:3000/email/";
+    const url = "http://localhost:3000/api/email/";
     try {
       console.log("tried to send");
       const response = await axios.post(url, { email: emailAdress });
       const ConfirmationAnswer: EmailAnswer = response.data;
       return ConfirmationAnswer;
     } catch (err) {
-      throw new Error(err.message + " Cannot connect to http://localhost:3000/email/");
+      throw new Error(err.message + " Cannot connect to http://localhost:3000/api/email/");
     }
   }
 }
