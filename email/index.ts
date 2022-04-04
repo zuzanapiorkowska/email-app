df562e4c082bf9970966498bc6aa8ab0e93e5418
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-04 13:53:49 +0200

    index file for email html config

--- email/index.ts
+++ email/index.ts
@@ -1,13 +1,11 @@
 import mjml2html from "mjml";
-
 /*
   Compile an mjml string
 */
 const options = {};
 
-export const htmlOutput = mjml2html(
-  `
-  <mjml>
+const email = `
+<mjml>
   <mj-head>
     <mj-attributes>
       <mj-text align="center" color="#555" />
@@ -17,26 +15,26 @@ export const htmlOutput = mjml2html(
   <mj-body background-color="#eee">
     <mj-section>
       <mj-column>
-        <mj-button background-color="#F63A4D" href="localhost:3000/api/rating?email=me&rating=1"> 1 </mj-button>
+        <mj-button background-color="#F63A4D" href="http://localhost:3000/api/rating?email=me@me.pl&rating=1"> 1 </mj-button>
       </mj-column>
       <mj-column>
-        <mj-button background-color="#F63A4D" href="localhost:3000/api/rating?email=me&rating=2"> 2 </mj-button>
+        <mj-button background-color="#F63A4D" href="http://localhost:3000/api/rating?email=me@me.pl&rating=2"> 2 </mj-button>
       </mj-column>
       <mj-column>
-        <mj-button background-color="#F63A4D" href="localhost:3000/api/rating?email=me&rating=3"> 3 </mj-button>
+        <mj-button background-color="#F63A4D" href="http://localhost:3000/api/rating?email=me@me.pl&rating=3"> 3 </mj-button>
       </mj-column>
       <mj-column>
-        <mj-button background-color="#F63A4D" href="localhost:3000/api/rating?email=me&rating=4"> 4 </mj-button>
+        <mj-button background-color="#F63A4D" href="http://localhost:3000/api/rating?email=me@me.pl&rating=4"> 4 </mj-button>
       </mj-column>
       <mj-column>
-        <mj-button background-color="#F63A4D" href="localhost:3000/api/rating?email=me&rating=5"> 5 </mj-button>
+        <mj-button background-color="#F63A4D" href="http://localhost:3000/api/rating?email=me@me.pl&rating=5"> 5 </mj-button>
       </mj-column>
     </mj-section>
   </mj-body>
 </mjml>
-`,
-  options
-);
+`;
+
+export const htmlOutput = mjml2html(email, options);
 
 /*
   Print the responsive HTML generated and MJML errors if any
