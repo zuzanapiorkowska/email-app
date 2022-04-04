f68028cb3cf3a77cbf1c1c293b6b6a40bc02e84c
tomek <tomaszpawelkowalski@gmail.com>
2022-04-04 15:07:47 +0200

    Added tests for review endpoint.

---
+++ test/feedback.test.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+
+describe('Validating reviews', () => {
+    describe('Name of the group', () => {
+
+        it('should return values from form', async () => {
+
+            const response = await axios.get('http://localhost:3000/api/rating?email=xyz@gmail.com&rating=5');
+            expect(response.data.statusCode).toBe(200);
+            expect(response.data.message).toBe('Thank you for your feedback!');
+        });
+    });
+    describe('Invalid values in review', () => {
+        it('should throw an error  wrong out of scale', async () => {
+
+            await axios.get('http://localhost:3000/api/rating?email=xy@gmail.com&rating=6').then((response) => { fail('should not pass') }).catch((err) => { expect(err.response.status).toBe(400) });
+        });
+
+        it('should throw an error rating wrong email out of scale', async () => {
+
+            await axios.get('http://localhost:3000/api/rating?email=xy&rating=5').then((response) => { fail('should not pass') }).catch((err) => { expect(err.response.status).toBe(400) });
+
+        });
+    });
+
+
+});
