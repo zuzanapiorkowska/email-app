6e7341a383b4266d013478cfece4faa79d92fece
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-04 10:12:57 +0200

    adding nodemailer and sendgrid

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -2,6 +2,8 @@
 import { IsEmail, validate } from "class-validator";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailAnswer, EmailForm } from "../../interfaces/email";
+import nodemailer from "nodemailer";
+// import sgTransport from "nodemailer-sendgrid-transport";
 
 export default function handler(
   req: NextApiRequest,
@@ -11,6 +13,30 @@ export default function handler(
     @IsEmail()
     email!: string;
   }
+
+  let transporter = nodemailer.createTransport({
+    service: "SendGrid",
+    auth: {
+      user: "",
+      pass: process.env.SENDGRID_KEY,
+    },
+  });
+
+  const email = {
+    from: "awesome@bar.com",
+    to: "sesovi9004@royins.com",
+    subject: "Hello",
+    text: "Hello world",
+    html: "<b>Hello world</b>",
+  };
+
+  transporter.sendMail(email, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent: " + info.response);
+    }
+  });
 
   const body: EmailForm = req.body;
   let emailValidation = new Email();
