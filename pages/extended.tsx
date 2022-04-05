b66265b67fdb55dd1cd4ec175c73a99915f4a344
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-05 09:25:23 +0200

    fix styles

--- pages/extended.tsx
+++ pages/extended.tsx
@@ -3,7 +3,7 @@ import { QuestionBox } from "../components/questionnaire/QuestionBox";
 
 function Extended() {
   return (
-    <div className="main-box">
+    <div className="main-box-big">
       <QuestionBox />
     </div>
   );
