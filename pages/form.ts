20f4a2d24e551145ea02867f7cd23e04c38f5275
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-06 10:06:35 +0200

    add validations

--- pages/form.ts
+++
@@ -1,4 +0,0 @@
-//użyć react hook form i formularz z polami zapisać za jego pomocą
-//jak się definiuje pola, które są arrayami?
-//z walidacji mamy react hook form class validator github
-
--- test/validate-request.test.ts
+++ test/validate-request.test.ts
@@ -9,11 +9,6 @@ function generateRequest(overrides: { email?: any; answers?: any }) {
 
   return request;
 }
-
-// test('passes with correct request', async () => {
-//   const request = generateRequest({});
-//   expect(await validate(request)).toEqual([])
-
 test("passes with correct request", async () => {
   const request = generateRequest({});
   expect(await validate(request)).toEqual([]);
@@ -22,9 +17,6 @@ test("passes with correct request", async () => {
 test("fails without email", async () => {
   const request = generateRequest({ email: null });
   expect(await validate(request)).toMatchSnapshot();
-  //jak odpalimy za pierwszym razem, to generuje snapshot, za każdym następnym sprawdza czy jest zgodny ze snapshotem, a jak się nie zgadza, to mówi coś typu "ej brakuje tutaj takiej i takiej linijki)"
-  //można odpalić z flagą --u, to wtedy update'uje snapshoty
-  //jest też toMatchInlineSnapshot(), ale to się czasem wywala :d wtedy wrzuca pod testem ten snapshot
 });
 
 test("fails with incorrect email", async () => {
@@ -35,8 +27,6 @@ test("fails with incorrect email", async () => {
 test("fails with empty answers", async () => {
   const request = generateRequest({ answers: null });
   expect(await validate(request)).toEqual([
-    //moglibyśmy spróbować w tym arrayu cały ten obiekt, co chcemy, ale możemy sobie wybrać tylko niektóre pola, np. "{constrains: {isEmail: "email must be an email", isString: "email must be a string"}, property {email}}"
-    // może być też partial matcher, np. array, w którym pierwszy ma być obiektem, który zawiera takie pola jak... wtedy będzie
     expect.objectContaining({
       constraints: {
         isEmail: "email must be an email",
