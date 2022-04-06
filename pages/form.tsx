c0c620dfefa1f80158effdd6598df762cf17227c
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-06 11:59:08 +0200

    add question mapping

--- pages/form.tsx
+++ pages/form.tsx
@@ -8,14 +8,27 @@ export default function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data: any) => console.log(data);
 
+  const QAndA = [{
+    question: "pytanie pierwsze",
+    id: "pies",
+    answers: [1, 2, 3, 4, 5]
+  },
+  {
+    question: "pytanie drugie",
+    id: "kot",
+    answers: [1, 2, 3, 4, 5]
+  }]
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-
-      <input {...register("pies")} type="radio" value="1" />
-      <input {...register("pies")} type="radio" value="2" />
-      <input {...register("pies")} type="radio" value="3" />
-      <input {...register("pies")} type="radio" value="4" />
-      <input {...register("pies")} type="radio" value="5" />
+      {QAndA.map(qa => {
+        return <>
+        <p>{qa.question}</p>
+        {qa.answers.map(answer=> {
+          return <input {...register(qa.id, {required: true})} type="radio" value={answer} />
+        })}
+        </>
+      })}
 
       <input type="submit" />
     </form>
