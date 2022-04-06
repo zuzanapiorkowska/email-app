bed208d6ba200e3029abb796bf8c844c17ae1d14
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-06 11:42:57 +0200

    fix hook

--- pages/form.tsx
+++ pages/form.tsx
@@ -7,16 +7,15 @@ import { useForm } from "react-hook-form";
 export default function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data: any) => console.log(data);
-  console.log(errors);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
 
-      <input {...register} type="radio" value="1" />
-      <input {...register} type="radio" value="2" />
-      <input {...register} type="radio" value="3" />
-      <input {...register} type="radio" value="4" />
-      <input {...register} type="radio" value="5" />
+      <input {...register("pies")} type="radio" value="1" />
+      <input {...register("pies")} type="radio" value="2" />
+      <input {...register("pies")} type="radio" value="3" />
+      <input {...register("pies")} type="radio" value="4" />
+      <input {...register("pies")} type="radio" value="5" />
 
       <input type="submit" />
     </form>
